2fbe22541ee4ebd0b918661f6982ae5081ffb213
gonghongyu <gonghongyu@58ganji-corp.com>
2017-11-23 18:01:45 +0800

    merge

--- fis-conf.js
+++ fis-conf.js
@@ -1,52 +1,52 @@
-
 var path = require('path')
-fis.set ("pname-ghy-path", path.join(path.resolve(__dirname, '..'),"fangapp-release/src/js/mod/poplib"));
-fis.match ('*', {
+fis.set("pname-ghy-path", path.join(path.resolve(__dirname, '..'), "fangapp-release/src/js/mod/poplib"));
+var apidir = path.join(path.resolve(__dirname, './api'));
+console.log(apidir)
+
+fis.match('*', {
     release: false
 });
 
 // ================编译模板文件 ==========
-fis.match ("*/view/(*).tpl", {
-    parser: fis.plugin ('jsptpl'),
+fis.match("*/view/(*).tpl", {
+    parser: fis.plugin('jsptpl'),
     rExt: '.js',
 })
-
 // ================编译模板文件中的scss ==========
-
-fis.match ('/src/{**.tpl:scss,**.jsp:scss,**.scss}', {
+fis.match('/src/{**.tpl:scss,**.jsp:scss,**.scss}', {
     rExt: 'css',
-    parser: [fis.plugin ('nodev8-scss')],
+    parser: [fis.plugin('nodev8-scss')],
 });
 
 // 发布设置
-fis.match ('/src/js/*/(*)_main.js', {
-    release: "$1.js"
+fis.match('/src/js/*/(*)_main.js', {
+    release: "$1"
 });
 
-fis.match ('/src/test/(*)', {
-    release: "/test/$1"
-});
+// fis.match('/src/test/(*)', {
+//     release: "/test/$1"
+// });
 
 
-
-// 发布到 fangapp-renzheng 中
-fis.media ('lou_dev').match ('/src/**/(lou_main.js)', {
-    deploy: fis.plugin ('local-deliver', {
-        to: fis.get ("pname-ghy-path"),
-    })
-})
-fis.media ('shenfen_dev').match ('/src/**/(shenfen_main.js)', {
-    deploy: fis.plugin ('local-deliver', {
-        to: fis.get ("pname-ghy-path"),
-    })
-})
-fis.media ('shenfenpop_dev').match ('/src/**/(shenfenPop_main.js)', {
-    deploy: fis.plugin ('local-deliver', {
-        to: fis.get ("pname-ghy-path"),
+fis.media('api').match('/src/js/*/(*)_main.js', {
+    // 设置发布目录
+    deploy: fis.plugin('local-deliver', {
+        to: apidir
     })
 })
 
-fis.set ('project.ignore', [
+fis.media('shenfen_dev').match('/src/**/(shenfen_main.js)', {
+    deploy: fis.plugin('local-deliver', {
+        to: fis.get("pname-ghy-path"),
+    })
+})
+fis.media('shenfenpop_dev').match('/src/**/(shenfenPop_main.js)', {
+    deploy: fis.plugin('local-deliver', {
+        to: fis.get("pname-ghy-path"),
+    })
+})
+
+fis.set('project.ignore', [
     '**/nbproject/**',
     'output/**',
     '**/bat/**',
---
+++ src/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by gonghongyu on 2017/11/23.
+ */
+__inline('lou_main.js');
+__inline('shenfen_main.js');
+__inline('shenfen_rp.js');
+
+module.exports = {
+    SF: API
+}
