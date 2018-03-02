62e91a2809d769a3315abe920b7e621bb2130103
ghy511024 <249398279@qq.com>
2018-03-02 15:31:08 +0800

    文案变更为租房-个人中心

--- api/mod/zhibotip.js
+++ api/mod/zhibotip.js
@@ -36,7 +36,7 @@ TPL.addStyle = TPL.addStyle || function (styleContent) {
     document.getElementsByTagName("head")[0].appendChild(styleNode);
 };
 (function (TPL) {
-    TPL.tplmap['zhibotip'] = '<div class="zhibo-tip-wrap" id="zhibo-tip-wrap"><div class="zhibo-tip-panel"><div class="zhibo-tip-title"><h3>58直播看房</h3><p>一次直播，多人同时看房，出租就是快</p></div><div class="zhibo-tip-body"><div class="item"><div class="pic pic1"></div><div class="tip">开启直播看房功能</div><div class="arrow"></div></div><div class="item"><div class="pic pic2"></div><div class="tip">等待租客预约</div><div class="arrow"></div></div><div class="item"><div class="pic pic3"></div><div class="tip mt-zbkf" >到“租房-我的-直播看房”去直播</div></div></div><div class="line"></div><div class="zhibo-more" id="zhibo-more">了解更多</div></div><div class="zhibo-close" id="zhibo-close"></div></div>'
+    TPL.tplmap['zhibotip'] = '<div class="zhibo-tip-wrap" id="zhibo-tip-wrap"><div class="zhibo-tip-panel"><div class="zhibo-tip-title"><h3>58直播看房</h3><p>一次直播，多人同时看房，出租就是快</p></div><div class="zhibo-tip-body"><div class="item"><div class="pic pic1"></div><div class="tip">开启直播看房功能</div><div class="arrow"></div></div><div class="item"><div class="pic pic2"></div><div class="tip">等待租客预约</div><div class="arrow"></div></div><div class="item"><div class="pic pic3"></div><div class="tip mt-zbkf" >到“租房-个人中心”去直播</div></div></div><div class="line"></div><div class="zhibo-more" id="zhibo-more">了解更多</div></div><div class="zhibo-close" id="zhibo-close"></div></div>'
 })(TPL);
    
 //begin insert static dom
