1ae15e6116989b075eb2b7e181cefc2b13de1c29
lipan04 <lipan04@58ganji.com>
2018-04-18 20:45:30 +0800

    test.js

--- test/demo/test.js
+++ test/demo/test.js
@@ -10,7 +10,25 @@ let min_instance = function (total, number, a) {
     let array_neg = new Array(number);
     for (let i = 0; i < number; i++) {
         array_pos[i] = (i + 1) * capacity;
-        array_pos[i] = -array_pos[i];
+        array_neg[i] = -array_pos[i];
     }
-
+    array_neg.reverse();
+    array_neg.push(0);
+    let array=new Array(2*number+1);
+    array=array_neg.concat(array_pos);
+    console.log(array)
+    for(let i=0;i<array.length;i++){
+        if(a==array[0]){
+            return array[0];
+        }
+        if(a<=array[i]){
+            if(Math.abs(array[i]-a)<=Math.abs(array[i-1]-a)){
+                return array[i];
+            }else{
+                return array[i-1]
+            }
+        }
+    }
+    return false;
 };
+min_instance(10,5,5.5);
